ff7e68e5e353472c302cb5a71e0e63883c30e005
JY <kims2606@gmail.com>
2020-11-26 22:25:49 +0900

    create project

---
+++ src/main/java/com/jy/study/udemy/jpahibernatespringboot/JpaHibernateSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jy.study.udemy.jpahibernatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateSpringbootApplication.class, args);
+    }
+
+}
