5dae2a32e65a88484a2580048524122daf27239a
JY <kims2606@gmail.com>
2020-11-28 11:58:56 +0900

    jdbcTemplate로 person 데이터 insert, update

--- src/main/java/com/jy/study/udemy/jpahibernatespringboot/JpaHibernateSpringbootApplication.java
+++ src/main/java/com/jy/study/udemy/jpahibernatespringboot/JpaHibernateSpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.jy.study.udemy.jpahibernatespringboot;
 
+import com.jy.study.udemy.jpahibernatespringboot.entity.Person;
 import com.jy.study.udemy.jpahibernatespringboot.jdbc.PersonJdbcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class JpaHibernateSpringbootApplication implements CommandLineRunner {
@@ -22,8 +25,19 @@ public class JpaHibernateSpringbootApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("All users -> {}", personJdbcDao.findAll());
-        logger.info("User id 10001 -> {}", personJdbcDao.findById(10001));
-        logger.info("Deleting 10002 -> No of Rows Deleted - {}", personJdbcDao.deleteById(10002));
+        logger.info("All users -> {}",
+                personJdbcDao.findAll());
+
+        logger.info("User id 10001 -> {}",
+                personJdbcDao.findById(10001));
+
+        logger.info("Deleting 10002 -> No of Rows Deleted - {}",
+                personJdbcDao.deleteById(10002));
+
+        logger.info("Inserting 10004 -> {}",
+                personJdbcDao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+
+        logger.info("Update 10003 -> {}",
+                personJdbcDao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
     }
 }
