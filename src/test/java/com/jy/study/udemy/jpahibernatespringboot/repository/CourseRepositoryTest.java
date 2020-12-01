058b9db6d9d9eeac4fd052a050057716d5b34de1
JY <kims2606@gmail.com>
2020-12-01 09:08:35 +0900

    CourseRepository findById 메서드 단위 테스트

---
+++ src/test/java/com/jy/study/udemy/jpahibernatespringboot/repository/CourseRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.jy.study.udemy.jpahibernatespringboot.repository;
+
+import com.jy.study.udemy.jpahibernatespringboot.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+    @Autowired
+    CourseRepository repository;
+
+    @Test
+    public void findById_basic() {
+        Course course = repository.findById(10001L);
+        assertEquals("JPA in 50 Steps", course.getName());
+    }
+
+}
