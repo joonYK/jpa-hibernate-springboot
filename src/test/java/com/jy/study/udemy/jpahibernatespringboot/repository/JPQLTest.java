cfcea9c1f9e3d9e21b87c4ccb85892575597969e
JY <kims2606@gmail.com>
2020-12-02 22:07:40 +0900

    JPQL 테스트

---
+++ src/test/java/com/jy/study/udemy/jpahibernatespringboot/repository/JPQLTest.java
@@ -0,0 +1,44 @@
+package com.jy.study.udemy.jpahibernatespringboot.repository;
+
+import com.jy.study.udemy.jpahibernatespringboot.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@SpringBootTest
+public class JPQLTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void jpql_basic() {
+        //From Course는 데이터베이스 테이블의 course가 아닌 자바의 엔티티 클래스.
+        Query query = em.createQuery("Select c From Course c");
+        List resultList = query.getResultList();
+        logger.info("Select c From Course c -> {}", resultList);
+    }
+
+    @Test
+    public void jpql_typed() {
+        TypedQuery<Course> query = em.createQuery("Select c From Course c", Course.class);
+        List<Course> resultList = query.getResultList();
+        logger.info("Select c From Course c -> {}", resultList);
+    }
+
+    @Test
+    public void jpql_where() {
+        TypedQuery<Course> query = em.createQuery("Select c From Course c where name like '%100 Steps'", Course.class);
+        List<Course> resultList = query.getResultList();
+        logger.info("Select c From Course c where name like '%100 Steps' -> {}", resultList);
+    }
+}
